45c8bd89d395bb4b5857e26115da70941c6309ba
Tsung-Yuan,Hsieh <dennesshen@gmail.com>
2022-07-27 00:22:40 +0800

    mac 20220727 00:22

---
+++ LeetCodePractice/src/main/java/study/LongestWord_524/Solution_2.java
@@ -0,0 +1,92 @@
+package study.LongestWord_524;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+524. Longest Word in Dictionary through Deleting：
+
+Given a string s and a string array dictionary, 
+return the longest string in the dictionary that can be formed by deleting some of the given string characters. 
+If there is more than one possible result, return the longest word with the smallest lexicographical order. 
+If there is no possible result, return the empty string.
+
+Example 1:
+Input: s = "abpcplea", dictionary = ["ale","apple","monkey","plea"]
+Output: "apple"
+
+Example 2:
+Input: s = "abpcplea", dictionary = ["a","b","c"]
+Output: "a"
+
+*/
+
+/**
+ * @author christinehsieh
+ */
+public class Solution_2 {
+	
+	public  String findLongestWord(String s, List<String> dictionary) {
+        Set<String> resultSet = new LinkedHashSet<>();
+        
+		for (String dictionary_string : dictionary) {
+			compare(s, dictionary_string, resultSet);
+		}
+		
+		String answer =
+		resultSet.stream().reduce((s1, s2) -> {
+												  if (s1.length() > s2.length()) {
+													  return s1;
+												  }else if (s1.length() < s2.length()) {
+													  return s2;
+												  }
+												  return compareLexicographicalOrder(s1, s2);
+													
+								  })
+						  .orElseGet(() -> "");
+		
+		return answer;
+    }
+	
+	private static void compare(String content, String target, Set<String> answerSet){
+		
+		int contentIndex = 0;
+		int targetIndex = 0;
+		
+		for (int i = 0; i < target.length(); i++) {
+			
+			char targetWord = target.charAt(i);
+			
+			for (int j = contentIndex; j < content.length(); j++) {
+				
+				if (targetWord == content.charAt(j)) {
+					contentIndex = j+1;
+					targetIndex +=1;
+					break;
+				}
+			}
+			
+			if (i == targetIndex) break;
+			
+		}
+		
+		if (targetIndex == target.length()) {
+			answerSet.add(target);
+		}
+	}
+	
+	
+	private static String compareLexicographicalOrder(String s1, String s2) {
+		
+		if (s1.compareTo(s2) > 0) {
+			return s2;
+		}else{
+			return s1;
+		}
+		
+	}
+	
+}
